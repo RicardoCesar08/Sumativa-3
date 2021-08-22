0f6b0005b05d1670c3b561c338e9e480535374e7
RicardoCesar08 <88217915+RicardoCesar08@users.noreply.github.com>
2021-08-21 21:41:12 -0500

    Add files via upload

---
+++ 1 Ecuacion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Elaborar una aplicación de línea de comandos en C que calcule la expresión de segundo grado: ax^2+bx+c=0*/
+
+void ecuacion(float*num1, float*num2, float*num3);
+
+int main() {
+    float num1;
+    float num2;
+    float num3;
+    printf("Ingresa tu primer numero porfavor: ");
+    scanf("%f", &num1);
+    printf("Ingresa tu segundo numero porfavor: ");
+    scanf("%f", &num2);
+    printf("Ingresa tu tercer numero porfavor: ");
+    scanf("%f", &num3);
+    ecuacion(&num1, &num2, &num3);
+    return 0;
+}
+
+void ecuacion(float*num1, float*num2, float*num3) {
+    float resultadoRaiz;
+    float resultado1;
+    float resultado2;
+    resultadoRaiz= sqrt(pow(*num2, 2)-((*num1) * (*num3) * 4));
+    resultado1 = (-(*num2) + (resultadoRaiz)) / (2 * (*num1));
+    resultado2 = (-(*num2) - (resultadoRaiz)) / (2 * (*num1));
+    printf("El resultado de X1 es: %f", resultado1);
+    printf("El resultado de X2 es: %f", resultado2);
+
+}
